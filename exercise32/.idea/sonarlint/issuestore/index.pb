bd9d4103065cb3d576a20be98ca441e15781bbea
Jay Jo <jkglaspey@gmail.com>
2021-09-30 18:09:15 -0400

    Pseudocode for Exercise 33

---
+++ exercise32/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution32.java,b\d\bd879f0038f65cacac80a75883d242ae15f31cbf
+\
+,src/test/java/baseline/RandomNumberTest.java,3\c\3ce6371e3dedd117ee10bdc1306de89ceefcc516
+X
+(src/main/java/baseline/RandomNumber.java,f\6\f6474d2548774ccae769ec80793879d3245db870
---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Glaspey
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution33 {
+
+    /*
+    Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again
+    later."
+     */
+
+    //create class Scanner
+    private static final Scanner in = new Scanner(System.in);
+
+    //create method to return a random number between 0 and 3
+    public static int generateRandomNumber() {
+        //return random number
+    }
+
+    //create method that prompts user to enter a string but not store it >:)
+    public static void askUserForQuestion() {
+        //print "What's your question?"
+    }
+
+    //create method that takes a list of strings and an index and prints the string at that index
+    public static void printRandom(String[] list, int index) {
+        //print the specific response
+    }
+
+    public static void main(String[] args) {
+
+        //create a list of responses
+        String[] list = new String[4];
+
+        //generate random number between 0 and 3
+        int randomIndex = generateRandomNumber();
+
+        //ask user to enter a question
+        askUserForQuestion();
+
+        //disregard the question and print random response
+        printRandom(list,randomIndex);
+    }
+}
